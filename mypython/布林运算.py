89be62e6fb929682293c6f7e4ffb5b75065e770a
unknown <you@example.com>
2019-03-15 15:33:16 +0800

    布尔运算

---
+++ mypython/布林运算.py
@@ -0,0 +1,15 @@
+print(True and True)  
+print(True and False)
+print(False and False)  
+print(True or True)  
+print(True or False)
+print(False or False)
+
+True and False#错误 无法执行
+print(0 and 1) #0
+print(2 and 3) #3
+print(2 or 3) #2
+
+print(2<3 and 3) #3
+
+print(2>3 and 3) #False
